debb74ac4f3cd3bd1b47720d71b3017654f879d1
github username <github email address>
2019-12-26 14:05:18 -0800

    bug issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,23 @@ class PostsController < ApplicationController
         erb :'posts/index'
     end
 
+    get '/posts/new' do
+        authenticate
+        erb :'posts/new'
+    end
+
+    post '/posts' do
+        authenticate
+        u = current_user
+        u.posts.build(content: params[:content])
+        if u.save
+            redirect '/posts'
+        else
+            @messages = "There was an issue"
+            erb :'/posts/new'
+        end
+    end
+
 
 
 
